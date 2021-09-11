9aa9556dc844daa96e85e5a752ca1d3f1951f3d8
seayounHan <helious4me@gmail.com>
2021-09-11 12:23:58 +0900

    test

--- order/src/main/java/food/delivery/work/OrderRepository.java
+++ order/src/main/java/food/delivery/work/OrderRepository.java
@@ -1,10 +1,14 @@
 package food.delivery.work;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel="orders", path="orders")
 public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
+	
+	List<Order> findByClassId(Long orderId);
 
 
 }
