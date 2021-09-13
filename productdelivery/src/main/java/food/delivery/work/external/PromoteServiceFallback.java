379a6119617f45e0810eee6082ea883a27018a86
seayounHan <helious4me@gmail.com>
2021-09-14 01:22:39 +0900

    add new files

---
+++ productdelivery/src/main/java/food/delivery/work/external/PromoteServiceFallback.java
@@ -0,0 +1,16 @@
+package food.delivery.work.external;
+
+import org.springframework.stereotype.Component;
+
+import food.delivery.work.Promote;
+
+@Component
+public class PromoteServiceFallback implements PromoteService {
+    @Override
+    public boolean publishCoupon(Promote promote) {
+        //do nothing if you want to forgive it
+
+        System.out.println("Circuit breaker has been opened. Fallback returned instead.");
+        return false;
+    }
+}
